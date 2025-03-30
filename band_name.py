985162958cc5b258047d4306ebf001cf5372333b
gyandeepkintali <gyan.kintali@gmail.com>
2025-03-30 11:50:49 -0700

    Add files via upload

---
+++ band_name.py
@@ -0,0 +1,8 @@
+
+# Online Python - IDE, Editor, Compiler, Interpreter
+
+print("welcome to band name generator")
+
+city = input("what is the name of your city: ")
+pet = input("what is the name of your pet: ")
+print("Your band name could be: " + city + " " + pet)
